57a9d32eb71bff98c6df9c01ff2994dd23829078
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-06 15:58:19 -0700

    conditionals

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - The entry head of the program
+ * if - conditionals
+ * else if - conditional check
+ * Return: outputs result which is an int as defined
+ */
+int main(void)
+{
+	int n;
+	int last_d;
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	last_d = n % 10;
+	/* your code goes there */
+	if (last_d > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	else if (last_d == 0)
+		printf("last digit of %d is %d and is 0\n", n, last_d);
+	else
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	return(0);
+}

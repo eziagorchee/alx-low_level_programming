39bb0dc672eada28f917855b891c5d3294a1961a
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-06 17:22:13 -0700

    conditionals

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: outps program result type, int
+ */
+
+int main(void)
+{
+	char start;
+
+	start = 'a';
+
+	while (start <= 'z')
+	{
+		if ((start != 'q')
+			&& (start != 'e'))
+		{
+			putchar(start);
+			start++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

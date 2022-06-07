a36c1a9e0a9391e08d8416ad56647290cfbd9527
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-06 18:13:36 -0700

    counting 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The entry point
+ * Return: outputs int result
+ */
+
+int main(void)
+{
+	int start;
+
+	start = 0;
+
+	while (start < 10)
+	{
+		printf("%d", start);
+		start++;
+	}
+	printf("\n");
+	return (0);
+}

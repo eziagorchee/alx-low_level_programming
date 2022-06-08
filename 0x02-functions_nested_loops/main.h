5675661734df2853cca8f95b93cca138103661bd
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-07 21:59:29 -0700

    function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/**
+ * File: main.h
+ * Description: Custom function prototype declarations
+ */
+
+/**
+ * _putchar - writes a single character
+ * Return: outputs int return type
+ */
+int _putchar(char);

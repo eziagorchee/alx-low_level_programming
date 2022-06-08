5571d7a23366750d7533592bae3c72114c1e3ac4
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-08 09:54:41 -0700

    print alpabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -The entry point of the program
+ * Return: outputs result
+ */
+
+int main(void)
+{
+	char start = 'a';
+	print_alphabet(void)
+	{
+		while (start <= 'z')
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+		return (void);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,20 @@
+#ifndef main.h
+#define (main.h)
 /**
  * File: main.h
  * Description: Custom function prototype declarations
  */
 
 /**
- * _putchar - writes a single character
- * Return: outputs int return type
+ ** _putchar - writes a single character
+ * Return: 1
+ * @char: represents single English letter
  */
 int _putchar(char);
+
+/**
+ * print_alphabet: this displays lower case alphabets
+ * Return: void
+ */
+void print_alphabet(void);
+#endif

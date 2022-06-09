38793d36a14e0fe512d5c096b6742dcba1404be4
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-09 13:41:03 -0700

    debug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,14 +14,22 @@ int largest_number(int a, int b, int c)
 	if (a > b)
 	{
 		if (a > c)
+		{
 			largest = a;
+		}
 		else
+		{
 			largest = c;
+		}
 	}
 	else if (b > c)
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
+	}
 	
 	return (largest);
 }

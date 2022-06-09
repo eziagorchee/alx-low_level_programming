752d308d5882f000389dba6701e29e549622abd5
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-09 02:48:47 -0700

    debug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,13 +6,14 @@
  */
 void print_to_98(int n)
 {
+	int loop;
 	if (n == 0 || n == 98)
 	{
 		_putchar(n);
 		_putchar(',');
 		_putchar(' ');
 	}
-	for (int loop = n; loop < 98; loop++)
+	for (loop = n; loop < 98; loop++)
 	{
 		_putchar(n);
 		_putchar(',');

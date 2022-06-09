e303aa732942a0c38a2fd03d611cc8e6d430a028
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-08 21:18:30 -0700

    loop and list a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: displays alphabets
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char ah = 'a';
+	int count = 0;
+
+	while (count < 10)
+	{
+		while (ah <= 'z')
+		{
+			_putchar(ah);
+			ah++;
+		}
+		_putchar('\n');
+		count++;
+	}
+	return (void);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,4 +17,10 @@ int _putchar(char c);
  * Return: void
  */
 void print_alphabet(void);
+
+/**
+ * print_alphabet_x10: prints alphabets
+ * Return: void
+ */
+void print_alphabet_x10(void);
 #endif

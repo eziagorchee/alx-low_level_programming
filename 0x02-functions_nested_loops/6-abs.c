5103838817792473e09ea2e45e2e3b27dd0d58ce
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-08 22:37:03 -0700

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs -entry point
+ * @num: Number to be converted
+ * Return: returns 1;
+ */
+int _abs(int num)
+{
+	num = return (abs(num));
+	_putchar(num);
+	return (1);
+}

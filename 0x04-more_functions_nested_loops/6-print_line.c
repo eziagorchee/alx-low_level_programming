7333815a25560874c77fe9a825866583f6e3fb6e
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-10 10:03:38 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - function name
+ * @n: number of times the loop would run
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n);
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}

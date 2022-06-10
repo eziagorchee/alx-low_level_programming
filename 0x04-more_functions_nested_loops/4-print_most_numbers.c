4660192d554cec5d28538894064eede2ff01128e
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-10 06:53:25 -0700

    debug

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,8 @@ void print_most_numbers(void)
 
 	while (i <= '9')
 	{
-		if (i == '2' && i == '4')
-			continue;
-		_putchar(i);
+		if (i != '2' || i != '4')
+			_putchar(i);
 		i++;
 	}
 	_putchar('\n');

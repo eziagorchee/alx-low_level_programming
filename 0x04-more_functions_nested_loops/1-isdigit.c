2bc8237827f4585a02501bf2141ba465648e507f
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-10 05:21:41 -0700

    function to check for numbers 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - The function name
+ * @c: The number to be checked
+ * Return: return type is int
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@
  */
 
 int _isupper(int c);
+int _isdigit(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);

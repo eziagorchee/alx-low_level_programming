f325918469d671f2265ba675bb1c59fca80a900d
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-24 13:24:21 -0700

    update value using pointer variables

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - function name
+ * @n: function parameter
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * reset_to_98 - The function to reset
+ * @n: pointer variable
+ */
+void reset_to_98(int *n);
+#endif

5d4939f7a7a5d07534e6ea61f89def79d90859a2
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-25 05:40:14 -0700

    implement custom strlen() function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function name
+ * @s: parameter
+ * Return: lenght of string of type int
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (len != 0)
+		len++;
+	return (len);
+}

f347546ba8bd41ab8fd75783822125916918dad2
Chiagozie Eziagor <eziagorchy@gmail.com>
2022-06-28 16:03:56 -0700

    implement custom strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - function name
+ * @dest: first character
+ * @src: second character
+ * Return: returns pointerto dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _strcat - function name
+ * @dest: destination
+ * @src: source string to be merged with dest
+ * Return: pointer to dest chracter
+ */
+char *_strcat(char *dest, char *src);
+#endif
